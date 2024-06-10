7d056d4c14ec75e36e92f67f5f2d76fb3c892821
AlejandroZBarajas <falejandrozb@gmail.com>
2024-06-09 23:52:42 -0600

    [shuffled-doublePrint]

--- src/components/organism/Board.jsx
+++ src/components/organism/Board.jsx
@@ -12,13 +12,13 @@ function shuffleArray(array) {
 }
 
 function Board(props) {
-    // Duplicar las tarjetas
+
     const duplicatedCards = ogArray.card.flatMap((card, index) => ([
         { ...card, id: `${index}-1` },
         { ...card, id: `${index}-2` }
     ]));
 
-    // Mezclar las tarjetas de forma aleatoria
+
     const shuffledCards = shuffleArray(duplicatedCards);
 
     return (
@@ -38,45 +38,3 @@ function Board(props) {
 
 export default Board;
 
-
-
-/* import Card from "../molecule/Card";
-import ogArray from "../../data/Array"
-
-import "./Board.css"
-
-function Board(props){
-
-    return(
-        <div id="boardCss">
-            {
-                 ogArray.card.flatMap((card, index) => ([
-                    <Card 
-                        key={`${index}-1`} 
-                        image={card.image} 
-                        title={card.title} 
-                    />,
-                    <Card 
-                        key={`${index}-2`} 
-                        image={card.image} 
-                        title={card.title} 
-                    />
-                ]))
-            }
-        </div>
-    )
-    }
-    
-    export default Board
-
-
-    function shuffle(array){
-        for(let i = ogArray.length -1; i>0; i--){
-            const j = Math.floor(Math.random() * (i +1))
-            [array[i], array[j]] = [array[j], array[i]]
-        }
-        return array
-    }
-
-        shuffle()
-        ogArray.card.map((card, index)=><Card key={index} image={card.image} title={card.title} ></Card>) */
